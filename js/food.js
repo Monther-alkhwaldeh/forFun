24f865e8ebf69a019461dc8adaaef95b444f49e4
Roya982 <roayarana@gmail.com>
2021-02-26 20:42:07 +0200

    game prep

--- js/food.js
+++ js/food.js
@@ -1,14 +1,15 @@
-const foodForm = document.getElementById("food-Form")
-const orders = document.getElementById("orders")
+'use strict';
+const foodForm = document.getElementById('food-Form');
+const orders = document.getElementById('orders');
 
 function Food(item, quantity) {
-    this.item = item;
-    this.quantity = quantity;
+  this.item = item;
+  this.quantity = quantity;
 
-    Food.all.push(this)
+  Food.all.push(this);
 }
 
 Food.all = [];
 
-console.log(Food.all)
+console.log(Food.all);
 
