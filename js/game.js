e945018c831c6424c21e815b9d399d62f599e127
Roya982 <roayarana@gmail.com>
2021-02-26 22:11:54 +0200

    i want to merge

--- js/game.js
+++ js/game.js
@@ -1,1 +1,26 @@
+'use strict';
+
+// function randomNumber (min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+const namesArr=['m1','m2','m3','m4','m5'];
+let imgPath=[];
+// console.log(imgPath);
+for (let i = 0; i < namesArr.length; i++) {
+  imgPath.push(`../imgs/gameImg/${namesArr[i]}.jpg`);
+}
+console.log(imgPath);
+
+const gameSec = document.getElementById('game-sec');
+
+for (let i = 0; i < namesArr.length; i++) {
+  const gameRow = document.createElement('tr');
+  gameSec.appendChild(gameRow);
+  const rowContent=document.createElement('img');
+  gameRow.appendChild(rowContent);
+  rowContent.src=imgPath[i];
+  gameRow.textContent=rowContent;
+  console.log(rowContent);
+}
 
