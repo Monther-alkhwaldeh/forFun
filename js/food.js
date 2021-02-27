ac9fbbbdcf621097b1848dc5127cc6ac2f2691f5
Ghofran Dayyat <YOUR EMAIL ADDRESS>
2021-02-27 16:10:46 +0200

    check

--- js/food.js
+++ js/food.js
@@ -8,38 +8,81 @@ const orders = document.getElementById('orders');
 function Food(item, quantity) {
   this.item = item;
   this.quantity = quantity;
-  this.price1=5;
-  this.price2=7;
-  this.price3=9;
 
   Food.all.push(this);
 }
 
 Food.all = [];
-console.log(Food.all);
 
 function handelTakeOrder(event) {
   event.preventDefault();
-  console.log(event.target);
-  const quantity = event.target.quantity.value;
+ 
+  const quantity = parseFloat(event.target.quantity.value);
   const item = event.target.item.value;
   new Food(item, quantity);
   renderOrder();
-}
 
+}
 
 foodForm.addEventListener('submit', handelTakeOrder);
 
+// function renderOrder() {
+//   const ulEl = document.getElementById('orders');
+//   ulEl.innerHTML = ''; //to remove last order
+//   let preQuantity=0;
+//   let preItem='';
+//   for (let i = 0; i < Food.all.length; i++) {
+//     if (preItem !== Food.all[i].item){
+//       const liEl = document.createElement('li');
+//       ulEl.appendChild(liEl);
+//       liEl.textContent = `Your Order : ${Food.all[i].quantity} of ${Food.all[i].item}`;
+//       preItem = Food.all[i].item;
+//       preQuantity +=Food.all[i].quantity;
+
+//     }else{
+//       const liEl = document.createElement('li');
+//       ulEl.appendChild(liEl);
+//       preQuantity += Food.all[i].quantity;
+//       liEl.textContent = `Your Order : ${preQuantity} of ${Food.all[i].item}`;
+
+//     }
+//   }
+// }
+// renderOrder();
+let preq1 = 0;
+let preq2 = 0;
+let preq3 = 0;
+
+
 function renderOrder() {
   const ulEl = document.getElementById('orders');
-  ulEl.innerHTML = ''; //to remove last order
+  ulEl.innerHTML = '';
+  for (let i = 0 ; i < Food.all.length ; i++) {
+    if (Food.all[i].item === 'Salmon' ) {
+      console.log(Food.all[i].quantity);
 
-  for (let i = 0; i < Food.all.length; i++) {
-    const liEl = document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent = `Your Order : ${Food.all[i].quantity} of ${Food.all[i].item}`;
+      preq1+=Food.all[i].quantity;
+      const liEl = document.createElement('li');
+      ulEl.appendChild(liEl);
+      liEl.textContent = `Your Order : ${preq1} of ${Food.all[i].item}`;
+
+    } else if (Food.all[i].item === 'Cheesecake' ) {
+
+      preq2 +=Food.all[i].quantity;
+      const liEl = document.createElement('li');
+      ulEl.appendChild(liEl);
+      liEl.textContent = `Your Order : ${preq2} of ${Food.all[i].item}`;
+    }else {
+
+      preq3 +=Food.all[i].quantity;
+      const liEl = document.createElement('li');
+      ulEl.appendChild(liEl);
+      liEl.textContent = `Your Order : ${preq3} of ${Food.all[i].item}`;
+
+    }
+
 
   }
 }
 renderOrder();
-// alert(Food.all[i]*)
+
