962e08027a80f810b3dcd66d8cd30374cb3578f5
abdalazzezzalsalahat <abdalazzezzalsalahat@icloud.com>
2021-02-27 17:28:42 +0200

    added some js code

--- js/game.js
+++ js/game.js
@@ -1,28 +1,84 @@
 'use strict';
 
-function randomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const namesArr=['m1','m2','m3','m4','m5'];
-let imgPath=[];
-for (let i = 0; i < namesArr.length; i++) {
-  imgPath.push(`../imgs/gameImg/${namesArr[i]}.jpg`);
-}
-
+const imgs = [
+  'GImg1',
+  'GImg2',
+  'GImg3',
+  'GImg4',
+  'GImg5',
+  'GImg6' ];
+const imgPath = './imgs/gameImg/';
+const imgExt = '.png';
 const gameTable = document.getElementById('game-table');
-let counter = 3;
-
-for (let i = 0; i <= counter; i++) {
-  const gameRow = document.createElement('tr');
-  for (let i = 0; i <=4 ; i++) {
-    const gameTd = document.createElement('td');
-    gameTable.appendChild(gameRow);
-    gameRow.appendChild(gameTd);
-    const tdImg=document.createElement('img');
-    gameTd.appendChild(tdImg);
-    tdImg.src=imgPath[i];
+const tries = 3;
+const colms = 4;
+const rows = (imgs.length/2);
+const resBtn = document.getElementById('resBtn');
+const retryBtn = document.getElementById('retryBtn');
+//const saveBtn = document.getElementById('saveBtn');
+const matches = imgs.length;
+function randmIndex (arry) {
+  for (let i = arry.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [ arry[i], arry[j] ] = [ arry[j], arry[i] ];
   }
+  return arry;
+}
+
+function swich(){
+  /* switch between the text and the game */
+  document.getElementById('about').hidden = true;
+  document.getElementById('playBtn').hidden = true;
+  document.getElementById('match-game').hidden = false;
+}
+
+function fillTable() {
 
 }
+
+function Game (userName) {
+  this.userName = userName;
+  Game.prototype.rndrBody = function() {
+    const tblBody = document.getElementById('tbl-body');
+    for (let i = 0; i < rows; i++){
+      const tBodyRow = document.createElement('tr');
+      tblBody.appendChild(tBodyRow);
+      for (let c = 0; c <= colms ; c++){
+        const rand = Math.floor(randmIndex(0, imgs.length-1));
+        const tBdyData = document.createElement('td');
+        tBodyRow.appendChild(tBdyData);
+        const img = document.createElement('img');
+        tBdyData.appendChild(img);
+        img.src = `${imgPath}${imgs[rand]}${imgExt}`;
+      }
+    }
+  };
+
+}
+const un = new Game ('azooz');
+un.rndrBody();
+
+// while (tries <= 5){
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
