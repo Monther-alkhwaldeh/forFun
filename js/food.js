626bd40a4c24e4404b5f7fafc966bb0eb5819b57
Ghofran Dayyat <YOUR EMAIL ADDRESS>
2021-03-01 22:29:53 +0200

    test

--- js/food.js
+++ js/food.js
@@ -1,35 +1,27 @@
 'use strict';
 const foodForm = document.getElementById('food-Form');
 const orders = document.getElementById('orders');
-
 function Food(item, quantity) {
   this.item = item;
   this.quantity = quantity;
   Food.all.push(this);
   localStorage.setItem('orders', JSON.stringify(Food.all));
 }
-
 function retrieve() {
   if (localStorage.length > 0) {
     Food.all = JSON.parse(localStorage.getItem('orders'));
-
     renderOrder();
   }
-
 }
 Food.all = [];
-
 function handelTakeOrder(event) {
   event.preventDefault();
   const quantity = parseFloat(event.target.quantity.value);
   const item = event.target.item.value;
   new Food(item, quantity);
   renderOrder();
-
 }
-
 foodForm.addEventListener('submit', handelTakeOrder);
-
 function renderOrder() {
   const ulEl = document.getElementById('orders');
   ulEl.innerHTML = ''; //to remove last order
@@ -50,42 +42,24 @@ function renderOrder() {
       preQuantity += Food.all[i].quantity;
       liEl.textContent = `Your Order : ${preQuantity} of ${Food.all[i].item}`;
     }
-
   }
 }
-
-renderOrder();
-
-
-
-
 function MyFunction(recipe) {
   this.recipe = recipe;
   this.x = document.getElementById(this.recipe);
-
 }
-
 MyFunction.prototype.render = function () {
   if (this.x.style.display === 'block') {
     this.x.style.display = 'none';
   } else {
     this.x.style.display = 'block';
   }
-
 };
-
-
 let resipe1 = new MyFunction('resipePara1');
 let resipe2 = new MyFunction('resipePara2');
 let resipe3 = new MyFunction('resipePara3');
 let resipe4 = new MyFunction('resipePara4');
 let resipe5 = new MyFunction('resipePara5');
 let resipe6 = new MyFunction('resipePara6');
-
-
-
-
 retrieve();
 renderOrder();
-
-let x =0;
