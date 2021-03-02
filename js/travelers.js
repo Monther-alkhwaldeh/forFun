a5fa45a15e3d2b992a7157fb80dda15abb88f224
norahmad95 <artistnor95@gmail.com>
2021-03-02 03:05:55 +0200

    norTaveler

--- js/travelers.js
+++ js/travelers.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+// debugger;
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -76,40 +76,42 @@ const swim = ['Bolivia', 'India', 'lake', 'park'];
 const camp = ['switzwrland', 'Norway', 'beren', 'alps'];
 const climb = ['Montana', 'Washington', 'Forde', 'gorgia'];
 const allAct = [swim, camp, climb]
-let lap = 12;
-let votes = 0
+let laps = 12;
+let votes = 0;
 
 
-const imgesction = document.getElementById('imgesction')
-const att = document.createAttribute("onclick");
+// const imgesction = document.getElementById('imgesction')
+// const att = document.createAttribute("onclick");
 
-function render() {
-    for (let i = 0; i < allAct.length; i++) {
-        for (let j = 0; j < allAct[i].length; j++) {
-            const imgFrame = document.createElement('section');
-            imgesction.appendChild(imgFrame);
-            imgFrame.onclick = removeImge();
-            const img = document.createElement('img');
-            imgFrame.appendChild(img);
-            img.src = './imgs/travelimg/' + allAct[i][j] + '.jpg';
-            img.alt = allAct[i][j];
-            img.title = allAct[i][j];
-            img.id = 'scal'
+// function render() {
+//     for (let i = 0; i < allAct.length; i++) {
+//         for (let j = 0; j < allAct[i].length; j++) {
+//             const imgFrame = document.createElement('section');
+//             imgesction.appendChild(imgFrame);
+//             imgFrame.onclick = removeImge();
+//             const img = document.createElement('img');
+//             imgFrame.appendChild(img);
+//             img.src = './imgs/travelimg/' + allAct[i][j] + '.jpg';
+//             img.alt = allAct[i][j];
+//             img.title = allAct[i][j];
+//             img.id = 'scal'
 
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-// imgesction.addEventListener('click', onclick);
-debugger;
+// // imgesction.addEventListener('click', onclick);
+// debugger;
 
-function removeImge() {
+// function removeImge() {
 
-    let x = document.getElementByid('scal')
-    x.style.display = 'none'
+//     let x = document.getElementByid('scal')
+//     x.style.display = 'none'
 
-}
+// }
+
+
 // function onclick(event) {
 //     laps--;
 
@@ -132,10 +134,10 @@ function removeImge() {
 
 // render();
 
-// const leftImage = document.getElementById('left-image');
-// const centerImage = document.getElementById('center-image');
-// const rightImage = document.getElementById('right-image');
-
+const leftImage = document.getElementById('left-image');
+const centerImage = document.getElementById('center-image');
+const rightImage = document.getElementById('right-image');
+const imagesSection = document.getElementById('imagesSection');
 // const resultsBtn = document.getElementById('btn');
 
 
@@ -163,60 +165,61 @@ function removeImge() {
 
 
 
-// function render() {
-//     const leftIndex = randomNumber(0, Taveler.all.length - 1);
-//     const centerIndex = randomNumber(leftIndex, Taveler.all.length - 1);
-//     const rightIndex = randomNumber(0, Taveler.all.length - 1);
-//     // left imge
-//     leftImage.src = Taveler.all[leftIndex].path;
-//     leftImage.title = Taveler.all[leftIndex].name;
-//     leftImage.alt = Taveler.all[leftIndex].name;
+function render() {
+    const leftIndex = randomNumber(0, swim.length - 1);
+    const centerIndex = randomNumber(0, climb.length - 1);
+    const rightIndex = randomNumber(0, camp.length - 1);
 
-//     // center imge
-//     centerImage.src = Taveler.all[centerIndex].path;
-//     centerImage.title = Taveler.all[centerIndex].name;
-//     centerImage.alt = Taveler.all[centerIndex].name;
-//     // right imge
-//     rightImage.src = Taveler.all[rightIndex].path;
-//     rightImage.title = Taveler.all[rightIndex].name;
-//     rightImage.alt = Taveler.all[rightIndex].name;
+    // left imge
+    leftImage.src = './imgs/travelimg/' + swim[leftIndex] + '.jpg';
+    leftImage.title = swim[leftIndex];
+    leftImage.alt = swim[leftIndex];
 
-//     for (let i = 0; i < Taveler.length; i++) {
-//         if (leftIndex === centerIndex || rightIndex === leftIndex || rightIndex === centerIndex) {
-//             render(Taveler.all)
-//         } else {
-//             break;
-//         }
-//     }
-// }
+    // center imge
+    centerImage.src = './imgs/travelimg/' + camp[centerIndex] + '.jpg';
+    centerImage.title = camp[centerIndex];
+    centerImage.alt = camp[centerIndex];
+    // right imge
+    rightImage.src = './imgs/travelimg/' + climb[rightIndex] + '.jpg';
+    rightImage.title = climb[rightIndex];
+    rightImage.alt = climb[rightIndex];
 
 
+    if (leftIndex === centerIndex || rightIndex === leftIndex || rightIndex === centerIndex) {
+        render()
+    } else {
 
-// imgesction.addEventListener('click', handleClick);
+    }
 
-// function handleClick(event) {
-//     laps--;
+}
 
-//     if (laps === 0) {
-//         imgesction.removeEventListener('click', handleClick);
-//         // createChart();
-//     } else {
-//         if (event.target.id !== 'imgesction') {
-//             console.log(event.target);
-//             for (let i = 0; i < Taveler.all.length; i++) {
-//                 if (Taveler.all[i].name === event.target.title) {
-//                     Taveler.all[i].votes++;
-//                 }
-//             }
-//             render();
+imagesSection.addEventListener('click', handleClick);
 
-//             console.log(Taveler.all);
-//         }
-//     }
-// }
-// render();
+function handleClick(event) {
+    laps--;
 
+    if (laps === 0) {
+        imagesSection.removeEventListener('click', handleClick);
+        // createChart();
+    } else {
+        if (event.target.id !== 'images-section') {
+            console.log(event.target);
+            for (let i = 0; i < allAct.length; i++) {
+                for (let j = 0; j < allAct[i].length; j++) {
+                    {
+                        if (allAct[i][j] === event.target) {
+                            allAct[i][j].votes++;
+                        }
+                    }
+                    render();
+                    console.log(allAct[i][j]);
+                }
+            }
+        }
+        render();
+    }
 
+}
 // function resultBtn(event) {
 //     for (let i = 0; i < names[i]; i++) {
 //         if (this.votes <= 11) {
@@ -239,4 +242,4 @@ render();
 // camp.render();
 // swim.render();
 
-// imgesction.addEventListener('click', handleClick);
+imagesSection.addEventListener('click', handleClick);
