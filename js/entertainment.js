578dfa921a7acba71c1b7afca02bc886d2056833
monther-alkhwaldeh <mmskbh.91@gmail.com>
2021-03-02 14:42:17 +0200

    monther

--- js/entertainment.js
+++ js/entertainment.js
@@ -77,17 +77,17 @@ function UserRate(nameU, rateN) {
 }
 UserRate.all = [];
 
-function rateRender(){
-  const divC=document.getElementById('userRate');
-  const rateU=document.createElement('ul');
+function rateRender() {
+  const divC = document.getElementById('userRate');
+  const rateU = document.createElement('ul');
   divC.appendChild(rateU);
-  
 
-  for(let i=0;i<UserRate.all.length;i++){
-    rateU.innerHTML='';
-    const rateLi=document.createElement('li');
+
+  for (let i = 0; i < UserRate.all.length; i++) {
+    rateU.innerHTML = '';
+    const rateLi = document.createElement('li');
     rateU.appendChild(rateLi);
-    rateLi.textContent=` Thank You ${UserRate.all[i].nameU} And your rate is : ${UserRate.all[i].rateN}`;
+    rateLi.textContent = ` Thank You ${UserRate.all[i].nameU} And your rate is : ${UserRate.all[i].rateN}`;
   }
 }
 function retriverate() {
@@ -111,61 +111,9 @@ function submitB(event) {
   event.target.reset();
   rateRender();
 
-  
+
 }
 formp.addEventListener('submit', submitB);
 rateRender();
 
-
-// -------------------------------------
-// const cForm=document.getElementById('contactform');
-// const contactus=document.createElement('h3');
-// cForm.appendChild(contactus);
-// contactus.textContent='Contact Us';
-// const div1=document.createElement('div');
-// cForm.appendChild(div1);
-// const name=document.createElement('input');
-// div1.appendChild(name);
-// name.type='text';
-// name.id='uName';
-// name.placeholder='Enter your Name';
-// const div2=document.createElement('div');
-// cForm.appendChild(div2);
-// const subject=document.createElement('input');
-// div2.appendChild(subject);
-// subject.type='text';
-// subject.id='subject';
-// subject.placeholder='Enter Subject';
-// const div3=document.createElement('div');
-// cForm.appendChild(div3);
-// const phone=document.createElement('input');
-// div3.appendChild(phone);
-// phone.type='text';
-// phone.id='phoneNumber';
-// phone.placeholder='Enter Your Phone Number';
-// const div4=document.createElement('div');
-// cForm.appendChild(div4);
-// const email=document.createElement('input');
-// div4.appendChild(email);
-// email.type='text';
-// email.id='email';
-// email.placeholder='Enter Your Email';
-
-// const div5=document.createElement('div');
-// cForm.appendChild(div5);
-// const message=document.createElement('input');
-// div5.appendChild(message);
-// message.type='text';
-// message.id='message';
-// message.placeholder='Enter Your Message';
-
-// const div6=document.createElement('div');
-// cForm.appendChild(div6);
-
-// const div4=document.createElement('div');
-// cForm.appendChild(div4);
-// const email=document.createElement('input');
-// div4.appendChild(email);
-// phone.id='email';
-// phone.placeholder='Enter Your Email';
 
